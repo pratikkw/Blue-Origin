db7071a6895ccc0c8dd0d21af2edb63b94c16881
Pratik Kesharwani <104644580+pratikkw@users.noreply.github.com>
2023-07-10 10:30:00 -0400

    upload

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,9 @@
 const open_btn = document.querySelector(".header__label");
 const slider = document.querySelector(".slider");
+const slider__shadow = document.querySelector(".slider-shadow");
 
 open_btn.addEventListener("click", function () {
   slider.classList.toggle("slider--active");
+  slider__shadow.classList.toggle("slider-shadow--active");
+  document.body.classList.toggle("lock-scroll");
 });
