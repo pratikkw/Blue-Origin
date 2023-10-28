b1c3290d02c584a23f112a4a3e102b20eea30c73
Pratik Kesharwani <104644580+pratikkw@users.noreply.github.com>
2023-10-28 07:33:30 -0400

    loader

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,13 @@
 const open_btn = document.querySelector(".header__label");
 const slider = document.querySelector(".slider");
 const slider__shadow = document.querySelector(".slider-shadow");
+const loader = document.querySelector(".loader");
+
+document.body.classList.toggle("lock-scroll");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+  document.querySelector("body").classList.toggle("lock-scroll");
+});
 
 open_btn.addEventListener("click", function () {
   slider.classList.toggle("slider--active");
